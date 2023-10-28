32d326cbd881f8dc8a03825ffd75c54db4c7a3ed
RAJENDRA PRASAD R <77789626+chinucod@users.noreply.github.com>
2023-10-28 09:57:25 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React , { useState } from 'react'
+
 import logo2 from '../images/logo2.png'
+import close from "../images/close.png"
+import menu from "../images/menu.png"
 const Header = () => {
+  const dropdown = document.querySelector('.nav-bar-res')
+  const click = document.querySelector('.icon')
+  const btn = document.querySelector('.btn')
+
+  
+  
+ 
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+  
   return (
     <div id='home'>
     <div className='fixed'>
@@ -12,8 +28,18 @@ const Header = () => {
               <li><a className='link' href='#projects'>Projects</a></li>
               <li><a className='link' href='#footer'>Contact</a></li>
               <li><a href='../resume/Rajendra_prasad_resume.pdf' download>Resume</a></li>
+        
             </ul>
+              <div className='btn' onClick={toggleDropdown}><img className='icon' src={isOpen ? close : menu}/></div>
       </div>
+      {isOpen &&
+      <div className='nav-bar-res'>
+              <li><a className='link' href='#me'>About</a></li>
+              <li><a className='link' href='#skills'>Skills</a></li>
+              <li><a className='link' href='#projects'>Projects</a></li>
+              <li><a className='link' href='#footer'>Contact</a></li>
+              <li><a href='../resume/Rajendra_prasad_resume.pdf' download>Download Resume</a></li>
+      </div>}
       </div>
       </div>
       
